5945f183ef8f48a4088b53fd2ac904923d421666
Marek Truska <truskamarek@centrum.sk>
2023-01-30 13:29:23 +0100

    logic for saving data

--- script.js
+++ script.js
@@ -9,35 +9,49 @@ const expenseItem = document.querySelector("#expensesPanel > .expenseItem");
 const weeksInfo = document.getElementById("weeksInfo");
 const newEventModal = document.getElementById("newEventModal");
 const backDrop = document.getElementById("backDrop");
+const newEventValue = document.getElementById("expenseValue");
+const newEventDate = document.getElementById("eventDate");
+const newEventDescription = document.getElementById("description");
 
 function load() {
   const dt = new Date();
   const currentMonth = dt.getMonth();
   const currentYear = dt.getFullYear();
 
-  month.innerText = `${dt.toLocaleDateString('en-us', { month: 'long' })} ${currentYear}`;
-
+  month.innerText = `${dt.toLocaleDateString("en-us", {
+    month: "long",
+  })} ${currentYear}`;
 
   const currentMonthWeekCount = weekCount(currentYear, currentMonth);
 
-  weeks.style.gridTemplateColumns = `repeat(${currentMonthWeekCount+1}, 1fr)`;
-  weeksInfo.style.gridTemplateColumns = `repeat(${currentMonthWeekCount+1}, 1fr)`;
+  weeks.style.gridTemplateColumns = `repeat(${currentMonthWeekCount + 1}, 1fr)`;
+  weeksInfo.style.gridTemplateColumns = `repeat(${
+    currentMonthWeekCount + 1
+  }, 1fr)`;
 
-  for (let i = 0; i < (currentMonthWeekCount+1) * 6; i++) {
+  for (let i = 1; i <= (currentMonthWeekCount + 1) * 6; i++) {
     const newWeek = document.createElement("div");
     const addNewItem = document.createElement("button");
     const zoomDetails = document.createElement("div");
 
-    newWeek.classList.add("newWeek");
-    addNewItem.classList.add("add");
-    zoomDetails.classList.add("fa-solid");
-    zoomDetails.classList.add("fa-magnifying-glass-plus");
+    if (
+      (i !== 0 && i % (currentMonthWeekCount + 1) === 0) ||
+      i > (currentMonthWeekCount + 1) * 6 - 6
+    ) {
+      newWeek.classList.add("newWeek");
+      weeks.appendChild(newWeek);
+    } else {
+      newWeek.classList.add("newWeek");
+      addNewItem.classList.add("add");
+      zoomDetails.classList.add("fa-solid");
+      zoomDetails.classList.add("fa-magnifying-glass-plus");
 
-    weeks.appendChild(newWeek);
-    newWeek.appendChild(addNewItem);
-    newWeek.appendChild(zoomDetails);
+      weeks.appendChild(newWeek);
+      newWeek.appendChild(addNewItem);
+      newWeek.appendChild(zoomDetails);
 
-    addNewItem.innerText = "+";
+      addNewItem.innerText = "+";
+    }
 
     newWeek.addEventListener("mouseover", () => {
       newWeek.classList.add("active");
@@ -46,16 +60,16 @@ function load() {
       newWeek.classList.remove("active");
     });
 
-    addNewItem.addEventListener('click', openModal)
+    addNewItem.addEventListener("click", openModal);
   }
 
-  for(let i = 0; i < currentMonthWeekCount + 1; i++) {
+  for (let i = 0; i < currentMonthWeekCount + 1; i++) {
     const weekNum = document.createElement("div");
     weekNum.classList.add("weekNum");
-    if(i === currentMonthWeekCount) {
-      weekNum.innerText = `Total`
+    if (i === currentMonthWeekCount) {
+      weekNum.innerText = `Total`;
     } else {
-    weekNum.innerText = `Week ${i+1}`
+      weekNum.innerText = `Week ${i + 1}`;
     }
     weeksInfo.appendChild(weekNum);
   }
@@ -71,8 +85,31 @@ function weekCount(year, month_number) {
 }
 
 function openModal() {
-  newEventModal.style.display = 'block';
-  backDrop.style.display = 'block';
+  newEventModal.style.display = "block";
+  backDrop.style.display = "block";
 }
 
+function saveEvent() {
+  events.push({
+    amount: newEventValue.value,
+    date: newEventDate.value,
+    description: newEventDescription.value,
+  });
+
+  localStorage.setItem("events", JSON.stringify(events));
+
+  closeEvent();
+}
+
+function closeEvent() {
+  backDrop.style.display = "none";
+  newEventModal.style.display = "none";
+}
+
+function buttons() {
+  document.getElementById("saveButton").addEventListener("click", saveEvent);
+  document.getElementById("cancelButton").addEventListener("click", closeEvent);
+}
+
+buttons();
 load();
